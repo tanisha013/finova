166486e8672bd8abad6043d5106c4cf738524b23
Tanisha Mukherjee <tanishamukherjee.013@gmail.com>
2025-12-02 21:35:22 +0530

    Landing page

---
+++ components/header.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { SignedOut, SignInButton, SignUpButton, SignedIn, UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+import Image from 'next/image'
+import { LayoutDashboard, PenBox } from "lucide-react"
+const Header = () => {
+  return (
+    <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+        <nav className='container mx-auto px-2 py-3 flex items-center justify-between'>
+           <Link href="/">
+           <Image 
+            src={"/logo.png"} 
+            alt='finova logo' 
+            height={60} 
+            width={150}
+            className='h-12 w-auto object-contain'/>
+           </Link>
+            <div className='flex items-center space-x-4'>
+                <SignedIn> 
+                    <Link href={"/dashboard"}>
+                        <button className="btn 'text-gray-600 hover:text-blue-600 flex items-center gap-2">
+                            <LayoutDashboard size={18}/>
+                            <span className='hidden md:inline'>Dashboard</span>
+                        </button>
+                    </Link>
+
+                    <Link href={"/transaction/create"}>
+                        <button className="btn bg-black text-white hover:bg-neutral-600  flex items-center gap-2">
+                            <PenBox size={18}/>
+                            <span className='hidden md:inline'>Add transaction</span>
+                        </button>
+                    </Link>
+                </SignedIn>
+            <SignedOut>
+                <SignInButton forceRedirectUrl='/dashboard'>
+                    <button className="btn btn-outline">Login</button>
+                </SignInButton>
+            </SignedOut>
+            <SignedIn>
+                <UserButton  
+                    appearance={{
+                        elements:{
+                            avatarBox:"w-10 h-10 "
+                        }
+                    }}
+                />
+            </SignedIn>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
---
+++ components/hero.jsx
@@ -0,0 +1,42 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+ 
+
+const HeroSection = () => {
+  const imageRef = useRef()
+  return (
+  <div className="pb-20 px-4">
+    <div className="container mx-autp text-center">
+      <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
+        Manage Your Finances <br/> with Intelligence
+      </h1>
+      <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
+        An AI-powered financial management platform that helps you track, analyze, and optimize your spending with real-time insights.
+      </p>
+      <div className="flex justify-center space-x-4">
+        <Link href="/dashboard">
+          <button className="btn btn-lg bg-black text-white rounded-xl shadow hover:bg-neutral-700 px-5">Get Started</button>
+        </Link>
+        <Link href="#features">
+          <button className="btn btn-lg px-5 rounded-xl">Explore Features</button>
+        </Link>
+      </div>
+      <div className="hero-image-wrapper">
+        <div>
+          <Image 
+            src='/banner.jpeg'  width={1200}
+            height={720}
+            alt="Dashboard Preview"
+            className="rounded-lg shadow-2xl border mx-auto"
+            priority
+          />  
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default HeroSection
