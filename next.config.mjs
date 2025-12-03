340b2bd5a386de8469e50c8b27dfee083d9161a7
Tanisha Mukherjee <tanishamukherjee.013@gmail.com>
2025-12-03 09:17:08 +0530

    Configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        pathname: "/**",
+      },
+    ],
+  },
+
   /* config options here */
   reactCompiler: true,
 };
